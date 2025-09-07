9ade253b689f8ce2c4f3dc1801bf84aa98328f28
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-09-07 03:29:19 +0300

    Quick invoice form

---
+++ lib/widgets/quick_invoice_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:responsev_dash_bord/widgets/title_text_feild.dart';
+
+class QuickInvoiceForm extends StatelessWidget {
+  const QuickInvoiceForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TitleTextFeild(
+                hint: 'Type customer name',
+                title: 'Customer name',
+              ),
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: TitleTextFeild(
+                hint: 'Type customer email',
+                title: 'Customer Email',
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 24),
+        Row(
+          children: [
+            Expanded(
+              child: TitleTextFeild(
+                hint: 'Type customer name',
+                title: 'Item name',
+              ),
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: TitleTextFeild(hint: 'USD', title: 'Item mount'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
